bedb357fd971b901a50f2ac655fceb81730cdc8e
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-05-16 10:15:12 +0200

    drawing book problem solved

---
+++ drawing-book/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the pageCount function below.
+ */
+func pageCount(n int32, p int32) int32 {
+	fmt.Println("number of pages: ", n)
+	fmt.Println("target page number: ", p)
+	if p == 1 {
+		return 0
+	} else if n%2 == 0 && p == n-1 {
+		return 1
+	}
+
+	return int32(math.Min(float64(p)/2, float64(n)/2-float64(p)/2))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	pTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	p := int32(pTemp)
+
+	result := pageCount(n, p)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
